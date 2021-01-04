b043ab4d9928870d29929d042226e170f8189a70
Diyane Fernando <wdrferna@uwaterloo.ca>
2021-01-03 23:53:42 -0500

    first commit to add template rest api

---
+++ src/main/java/com/webapp/WebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppApplication {
+
+	public static void main(String[] args) throws Throwable {
+		SpringApplication.run(WebAppApplication.class, args);
+	}
+
+}
